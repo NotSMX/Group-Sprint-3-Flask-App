71a75f5ab3be676d9b4ed9d24f7132ac10a377fa
Emely Pozo-Lin <empozo26@colby.edu>
2025-11-05 17:54:17 -0500

    Update app.py
    fixed creating db

--- app.py
+++ app.py
@@ -58,9 +58,6 @@ def create_submission():
     db.session.add(new_submission)
     db.session.commit()
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
